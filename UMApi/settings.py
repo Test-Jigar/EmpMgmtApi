e27ce2efa877d5c0850efa192151286b7d456909
iMac <imac@iMacs-iMac.local>
2022-09-14 11:44:39 +0530

    Updated

--- UMApi/settings.py
+++ UMApi/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'crud.apps.CrudConfig',
-    'corsheaders',
     'rest_framework.authtoken'
 ]
 
@@ -49,10 +48,8 @@ REST_FRAMEWORK = {
      ],
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
